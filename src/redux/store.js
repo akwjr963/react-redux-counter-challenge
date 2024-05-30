a4ac741848032b7a0705a49297a5c5d0ac8e349e
akwjr963 <akwjr963@daum.net>
2024-05-30 17:49:16 +0900

    challenge 13

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
+import counterReducer from './reducers';
+
+const store = createStore(counterReducer, applyMiddleware(thunk));
+
+export default store;
